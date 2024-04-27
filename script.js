38bd99d3743637f8e791850018532dcb6b76af94
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-04-27 16:46:17 +0530

    [add] grid

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var output = document.getElementById("sliderOutput");
 output.innerHTML = slider.value; // Display the default slider value
+var sliderValue = slider.value;
+
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  sliderValue = this.value;
+  output.innerHTML = sliderValue;
+
 }
+
+
+gridContainer.style.height = "600px"
+gridContainer.style.width = "600px"
+gridContainer.style.display = "flex";
+gridContainer.style.flexWrap = "wrap";
+
+
+// Create a 9x9 grid inside gridContainer div
+
+for (let i=0; i < 400; i++) {
+  let boxes = document.createElement("div");
+  boxes.classList.add("squares");
+  boxes.style.width = "30px";
+  boxes.style.height = "30px"
+  gridContainer.appendChild(boxes);
+}
+
+
+
+
